fbf758a9e4233f2ace182bfc18d896b9d0563ec8
Divya Rao <divya1aum@gmail.com>
2018-03-31 13:37:48 +0000

    automatically backed up by learn

--- lib/update.sql
+++ lib/update.sql
@@ -26,10 +26,7 @@ species TEXT,
 author_id INTEGER,
 series_id INTEGER);
 
-CREATE TABLE characters_books(
+CREATE TABLE character_books(
 id INTEGER PRIMARY KEY,
-name TEXT,
-motto TEXT,
-species TEXT,
-author_id INTEGER,
-series_id INTEGER);
+character_id INTEGER,
+book_id INTEGER);
