19e54b2c4035fa4949c06248ef85f492ef21380d
Divya Rao <divya1aum@gmail.com>
2018-03-31 13:50:50 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,5 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "select books.title, books.year from books where series_id=1 ORDER by books.year;"
+  "SELECT books.title, books.year from books where series_id=1 ORDER by books.year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
