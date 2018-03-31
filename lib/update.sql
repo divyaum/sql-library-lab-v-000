e2ec15c82cc56d489f68f4f6b44d4ce3a50a94ae
Divya Rao <divya1aum@gmail.com>
2018-03-31 13:36:44 +0000

    automatically backed up by learn

--- lib/update.sql
+++ lib/update.sql
@@ -5,23 +5,31 @@ author_id INTEGER,
 subgenre_id INTEGER);
 
 CREATE TABLE subgenres(
-id PRIMARY KEY INTEGER,
+id INTEGER PRIMARY KEY,
 name TEXT);
 
 CREATE TABLE authors(
-id PRIMARY KEY INTEGER,
+id INTEGER PRIMARY KEY,
 name TEXT);
 
 CREATE TABLE books(
-id PRIMARY KEY INTEGER,
+id INTEGER PRIMARY KEY,
 title TEXT,
 year INTEGER,
 series_id TEXT);
 
 CREATE TABLE characters(
-id PRIMARY KEY INTEGER,
+id INTEGER PRIMARY KEY,
 name TEXT,
 motto TEXT,
 species TEXT,
-author_id TEXT,
-series_id TEXT);
+author_id INTEGER,
+series_id INTEGER);
+
+CREATE TABLE characters(
+id INTEGER PRIMARY KEY,
+name TEXT,
+motto TEXT,
+species TEXT,
+author_id INTEGER,
+series_id INTEGER);
